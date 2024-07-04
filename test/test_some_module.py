0e2ba49cc7adddd6eb83dbeeccbf676e361e2a0f
SaavedraMd <saavedracesar700@gmail.com>
2024-07-04 09:34:44 -0600

    Update test_some_module.py

--- test/test_some_module.py
+++ test/test_some_module.py
@@ -1,1 +1,12 @@
+import unittest
+from src.some_module import add
+
+class TestSomeModule(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
+
+if __name__ == '__main__':
+    unittest.main()
 
